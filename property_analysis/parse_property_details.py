8222e22d16b7c9a8897af8e51df3081dd27125f3
Michael Lim <lim.s.michael@gmail.com>
2023-07-01 15:21:39 -0600

    feat: cleaned value strings to int

---
+++ property_analysis/parse_property_details.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def main(property_file):
+    property_details_df = pd.read_csv(property_file, index_col='Unnamed: 0')
+    for column in property_details_df.columns:
+        if type(property_details_df[column][0]) is not str:
+            pass
+        elif "$" in property_details_df[column][0]:
+            property_details_df[column] = property_details_df[column].str.replace("$", "")
+            property_details_df[column] = property_details_df[column].str.replace(",", "")
+            property_details_df[column] = pd.to_numeric(property_details_df[column])
+
+    property_details_df.to_csv(property_file)
+
+if __name__ == "__main__":
+    main(property_file= "./property_details.csv")
